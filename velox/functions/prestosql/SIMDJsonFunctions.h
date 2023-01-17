d12aa5907f386ac1e406eaa1e75b16aee32551c0
wanweiqiangintel <weiqiang.wan@intel.com>
2023-01-17 18:05:35 +0800

    add simdjson test Framework

---
+++ velox/functions/prestosql/SIMDJsonFunctions.h
@@ -0,0 +1,321 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "velox/functions/Macros.h"
+#include "velox/functions/UDFOutputString.h"
+#include "velox/functions/prestosql/json/JsonExtractor.h"
+#include "velox/functions/prestosql/types/JsonType.h"
+#include "velox/functions/Macros.h"
+#include "velox/functions/UDFOutputString.h"
+//#include "velox/functions/prestosql/json/ADBJsonExtractor.h"
+#include "velox/functions/prestosql/types/JsonType.h"
+//#include "velox/functions/prestosql/json/simdjson.h"
+#include <iostream>
+#include <sys/time.h>
+
+namespace facebook::velox::functions {
+
+template <typename T>
+struct SIMDJsonArrayContainsFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  template <typename TInput>
+  FOLLY_ALWAYS_INLINE bool
+  call(bool& result, const arg_type<Json>& json, const TInput& value) {
+/*    
+    std::string jsonData = json;
+    simdjson::ondemand::parser parser;
+    simdjson::ondemand::document jsonObj;
+    simdjson::padded_string padded_json(jsonData.data(),jsonData.length());
+    std::string jsonpath = "";
+    
+    try {
+      jsonObj = parser.iterate(padded_json);
+    }
+    catch(simdjson::simdjson_error& e) {
+      printf("error: Failed to parse json as document. error :%s\n",simdjson::error_message(e.error()));
+      return false;
+    }
+    
+    if (jsonObj.type() != simdjson::ondemand::json_type::array) {
+      return false;
+    }
+
+    result = false;
+    try{
+      for (auto v : jsonObj) {
+        if constexpr (std::is_same_v<TInput, bool>) {
+          if (v.type() == simdjson::ondemand::json_type::boolean && v.get_bool() == value) {
+            result = true;
+            break;
+          }
+        } else if constexpr (std::is_same_v<TInput, int64_t>) {
+          if (v.type() == simdjson::ondemand::json_type::number && ((v.get_number_type() == simdjson::ondemand::number_type::signed_integer && v.get_int64() == value) || (v.get_number_type() == simdjson::ondemand::number_type::unsigned_integer && v.get_uint64() == value))) {
+            result = true;
+            break;
+          }
+        } else if constexpr (std::is_same_v<TInput, double>) {
+          if (v.type() == simdjson::ondemand::json_type::number && v.get_number_type() == simdjson::ondemand::number_type::floating_point_number && v.get_double() == value) {
+            result = true;
+            break;
+          }
+        } else {
+          if (v.type() == simdjson::ondemand::json_type::string) {
+            std::string_view rlt = v.get_string();
+            std::string str_value = value.getString();
+            if(rlt.compare(str_value) == 0) {
+              result = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+    catch (simdjson::simdjson_error& e) {
+      if(e.error() != simdjson::INCORRECT_TYPE && e.error() != simdjson::NUMBER_ERROR) {
+        return false;
+      }
+    }
+    return true;
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonParseFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(
+      out_type<Varchar>& result,
+      const arg_type<Varchar>& json) {
+/*
+    std::string jsonData = json;
+
+    simdjson::dom::parser parser;
+    simdjson::dom::element jsonDoc;
+    simdjson::padded_string padded_json(jsonData.data(),jsonData.length());
+
+    try{
+      jsonDoc = parser.parse(padded_json);
+      std::string_view rlt_tmp = simdjson::to_string(jsonDoc);
+      std::string rlt(rlt_tmp);
+      UDFOutputString::assign(result, rlt);
+      return true;
+    } catch(simdjson::simdjson_error& e)
+    {
+      VELOX_USER_FAIL("Cannot convert '{}' to JSON", jsonData);
+      return false;
+    }
+
+    return false;
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonExtractScalarFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(
+      out_type<Varchar>& result,
+      const arg_type<Varchar>& json,
+      const arg_type<Varchar>& jsonPath) {
+/*    
+    std::string jsonData = json;
+    std::string jsonPathStr = jsonPath;
+
+    //ADBjsonExtractString is ondemand mode
+    auto extractResult = ADBjsonExtractScalar(jsonData, jsonPathStr);
+
+    if (extractResult.has_value()) {
+      UDFOutputString::assign(result, extractResult.value());
+      return true;
+    } else {
+      return false;
+    }
+    return false;
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonValidFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE void call(
+      int64_t& result,
+      const arg_type<Varchar>& json) {
+/*
+    std::string jsonData = json;
+    simdjson::dom::parser parser;
+    simdjson::dom::element jsonObj;
+    simdjson::padded_string padded_json(jsonData.data(),jsonData.length());
+    std::string jsonpath = "";
+    
+    try {
+      jsonObj = parser.parse(padded_json);
+      result = 1;
+    } catch(simdjson::simdjson_error& e)
+    {
+      printf("error: Failed to parse json as document. error :%s\n",simdjson::error_message(e.error()));
+      result = 0;
+    }
+*/
+  }
+};
+
+template <typename T>
+struct SIMDJsonArrayLengthFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(int64_t& result, const arg_type<Json>& json) {
+/*
+    std::string jsonData = json;
+    simdjson::ondemand::parser parser;
+    simdjson::ondemand::document jsonObj;
+    simdjson::padded_string padded_json(jsonData.data(),jsonData.length());
+    std::string jsonpath = "";
+    
+    try {
+      jsonObj = parser.iterate(padded_json);
+    }
+    catch(simdjson::simdjson_error& e) {
+      printf("error: Failed to parse json as document. error :%s\n",simdjson::error_message(e.error()));
+      return false;
+    }
+    
+    if (jsonObj.type() != simdjson::ondemand::json_type::array) {
+      return false;
+    }
+
+    result = 0;
+    try{
+      for (auto v : jsonObj) {
+        result++;
+      }
+      return true;
+    }
+    catch (simdjson::simdjson_error& e) {
+      printf("error: Failed to count array length. error :%s\n",simdjson::error_message(e.error()));
+    }
+    return false;
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonKeysFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(
+      out_type<Varchar>& result,
+      const arg_type<Json>& json) {
+/*
+    std::string jsonData = json;
+    simdjson::ondemand::parser parser;
+    simdjson::ondemand::document jsonObj;
+    simdjson::padded_string padded_json(jsonData.data(),jsonData.length());
+    
+    try {
+      jsonObj = parser.iterate(padded_json);
+    }
+    catch(simdjson::simdjson_error& e) {
+      printf("error: Failed to parse json as document. error :%s\n",simdjson::error_message(e.error()));
+      return false;
+    }
+    
+    if (jsonObj.type() != simdjson::ondemand::json_type::object) {
+      return false;
+    }
+
+    std::string rlt = "[";
+    int count = 0;
+    int objCnt = jsonObj.count_fields();
+    try{
+      for (auto field : jsonObj.get_object()) {
+        std::string_view tmp = field.unescaped_key();
+        rlt += "\""+std::string(tmp)+"\"";
+        if(++count != objCnt) {
+          rlt += ",";
+        }
+      }
+      rlt += "]";
+      UDFOutputString::assign(result, rlt);
+      return true;
+    }
+    catch (simdjson::simdjson_error& e) {
+      printf("error: Failed to find json key. error :%s\n",simdjson::error_message(e.error()));
+    }
+    return false;
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonKeysFunctionWithJsonPath {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(
+      out_type<Varchar>& result,
+      const arg_type<Json>& json,
+      const arg_type<Varchar>& jsonPath) {
+/*
+    std::string jsonData = json;
+    std::string jsonPathStr = jsonPath;
+
+    //ADBjsonExtractString is FULL parse mode
+    auto extractResult = ADBjsonKeysWithJsonPath(jsonData, jsonPathStr);
+
+    if (extractResult.has_value()) {
+      UDFOutputString::assign(result, extractResult.value());
+      return true;
+    } else {
+      std::cout<<"ADBJsonKeysFunctionWithJsonPath rlt is: null"<<std::endl;
+      return false;
+    }
+*/
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonLengthFunction {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(int64_t& result, const arg_type<Json>& json) {
+    // TODO: implement Json Length function with simdjson.
+    return false;
+  }
+};
+
+template <typename T>
+struct SIMDJsonLengthFunctionWithJsonPath {
+  VELOX_DEFINE_FUNCTION_TYPES(T);
+
+  FOLLY_ALWAYS_INLINE bool call(
+      int64_t& result,
+      const arg_type<Json>& json,
+      const arg_type<Varchar>& jsonPath) {
+    // TODO: implement JsonLengthFunctionWithJsonPath with simdjson.
+    return false;
+  }
+};
+
+} // namespace facebook::velox::functions
--- velox/functions/prestosql/registration/JsonFunctionsRegistration.cpp
+++ velox/functions/prestosql/registration/JsonFunctionsRegistration.cpp
@@ -16,6 +16,7 @@
 
 #include "velox/functions/Registerer.h"
 #include "velox/functions/prestosql/JsonFunctions.h"
+#include "velox/functions/prestosql/SIMDJsonFunctions.h"
 
 namespace facebook::velox::functions {
 void registerJsonFunctions() {
@@ -36,4 +37,45 @@ void registerJsonFunctions() {
       {"json_array_contains"});
 }
 
+void registerSIMDJsonFunctions() {
+  registerType("json", std::make_unique<const JsonTypeFactories>());
+
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Json, bool>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Json, int64_t>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Json, double>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Json, Varchar>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Varchar, bool>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Varchar, int64_t>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Varchar, double>(
+      {"simd_json_array_contains"});
+  registerFunction<SIMDJsonArrayContainsFunction, bool, Varchar, Varchar>(
+      {"simd_json_array_contains"});
+
+  registerFunction<SIMDJsonParseFunction, Varchar, Varchar>({"simd_json_parse"});
+  registerFunction<SIMDJsonExtractScalarFunction, Varchar, Varchar, Varchar>(
+      {"simd_json_extract_scalar"});
+  registerFunction<SIMDJsonValidFunction, int64_t, Varchar>({"simd_json_valid"});
+  {
+    registerFunction<SIMDJsonArrayLengthFunction, int64_t, Varchar>(
+        {"simd_json_array_length"});
+    registerFunction<SIMDJsonArrayLengthFunction, int64_t, Json>(
+        {"simd_json_array_length"});
+  }
+  {
+    registerFunction<SIMDJsonKeysFunction, Varchar, Json>({"simd_json_keys"});
+    registerFunction<SIMDJsonKeysFunctionWithJsonPath, Varchar, Json, Varchar>(
+        {"simd_json_keys"});
+  }
+  {
+    registerFunction<SIMDJsonLengthFunction, int64_t, Json>({"simd_json_length"});
+    registerFunction<SIMDJsonLengthFunctionWithJsonPath, int64_t, Json, Varchar>(
+        {"simd_json_length"});
+  }
+}
 } // namespace facebook::velox::functions
